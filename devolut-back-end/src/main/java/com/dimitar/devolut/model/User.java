7dc0604b663eb25b829c477c5be379a7e423fd4f
DSDimitrov19 <DSDimitrov19@codingburgas.bg>
2022-11-30 09:50:18 +0200

    Make Devolut Tag PK

--- devolut-back-end/src/main/java/com/dimitar/devolut/model/User.java
+++ devolut-back-end/src/main/java/com/dimitar/devolut/model/User.java
@@ -9,6 +9,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    @Column(unique = true)
     private String dTag;
 
     @Column(unique = true)
