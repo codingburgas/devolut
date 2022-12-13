47c8fab1fd7fb6ef9672f9dbb15d008066fb782e
DSDimitrov19 <DSDimitrov19@codingburgas.bg>
2022-12-13 11:00:24 +0200

    Customize a little more the header menu

--- devolut-front-end/pages/header.tsx
+++ devolut-front-end/pages/header.tsx
@@ -11,17 +11,19 @@ import {
   Icon,
 } from '@chakra-ui/react';
 import Link from 'next/link';
+import { AtSignIcon } from '@chakra-ui/icons';
 
-const Header = ({ session, signOut }) => {
-  const TransactionsIcon = () => (
+const Header = ({ session, router, signOut }) => {
+  const AccountIcon = () => (
     <Icon
-      viewBox="0 0 16 16"
-      fill="currentcolor"
+      viewBox="0 0 24 24"
+      stroke="currentcolor"
       aria-hidden="true"
       focusable="false"
+      fontSize={'lg'}
+      display={'block'}
     >
-      <path d="M1.92.506a.5.5 0 0 1 .434.14L3 1.293l.646-.647a.5.5 0 0 1 .708 0L5 1.293l.646-.647a.5.5 0 0 1 .708 0L7 1.293l.646-.647a.5.5 0 0 1 .708 0L9 1.293l.646-.647a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .801.13l.5 1A.5.5 0 0 1 15 2v12a.5.5 0 0 1-.053.224l-.5 1a.5.5 0 0 1-.8.13L13 14.707l-.646.647a.5.5 0 0 1-.708 0L11 14.707l-.646.647a.5.5 0 0 1-.708 0L9 14.707l-.646.647a.5.5 0 0 1-.708 0L7 14.707l-.646.647a.5.5 0 0 1-.708 0L5 14.707l-.646.647a.5.5 0 0 1-.708 0L3 14.707l-.646.647a.5.5 0 0 1-.801-.13l-.5-1A.5.5 0 0 1 1 14V2a.5.5 0 0 1 .053-.224l.5-1a.5.5 0 0 1 .367-.27zm.217 1.338L2 2.118v11.764l.137.274.51-.51a.5.5 0 0 1 .707 0l.646.647.646-.646a.5.5 0 0 1 .708 0l.646.646.646-.646a.5.5 0 0 1 .708 0l.646.646.646-.646a.5.5 0 0 1 .708 0l.646.646.646-.646a.5.5 0 0 1 .708 0l.646.646.646-.646a.5.5 0 0 1 .708 0l.509.509.137-.274V2.118l-.137-.274-.51.51a.5.5 0 0 1-.707 0L12 1.707l-.646.647a.5.5 0 0 1-.708 0L10 1.707l-.646.647a.5.5 0 0 1-.708 0L8 1.707l-.646.647a.5.5 0 0 1-.708 0L6 1.707l-.646.647a.5.5 0 0 1-.708 0L4 1.707l-.646.647a.5.5 0 0 1-.708 0l-.509-.51z"></path>
-      <path d="M3 4.5a.5.5 0 0 1 .5-.5h6a.5.5 0 1 1 0 1h-6a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 1 1 0 1h-6a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 1 1 0 1h-6a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5zm8-6a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5z"></path>
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path>
     </Icon>
   );
 
@@ -54,7 +56,8 @@ const Header = ({ session, signOut }) => {
           </Avatar>
         </MenuButton>
         <MenuList>
-          <MenuItem justifyContent={'space-between'} onClick={() => signOut()} fontWeight={'500'}>@{session?.user.dTag} <Text color={'red'}>Logout</Text></MenuItem>
+            <MenuItem icon={<AccountIcon/>} iconSpacing={'1'} onClick={() => router.push('/account')} fontWeight={'500'}>Account</MenuItem>
+            <MenuItem icon={<AtSignIcon fontSize={'lg'} display={'block'}/>} command={"Logout"} iconSpacing={'1'} onClick={() => signOut()} fontWeight={'500'}>{session?.user.dTag}</MenuItem>
         </MenuList>
       </Menu>
     </Flex>
