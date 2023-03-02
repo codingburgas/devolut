4db63125154753d81dd2fca8220cc628d551f84f
DSDimitrov19 <DSDimitrov19@codingburgas.bg>
2023-03-02 09:15:17 +0200

    Create vault repository

---
+++ devolut-back-end/src/main/java/com/dimitar/devolut/repository/VaultRepository.java
@@ -0,0 +1,10 @@
+package com.dimitar.devolut.repository;
+
+import com.dimitar.devolut.model.Vault;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VaultRepository extends JpaRepository<Vault, Integer> {
+    public Vault findByOwnerIdAndName(int ownerId, String name);
+}
