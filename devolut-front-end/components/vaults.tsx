0c73abba3468d81cede0efcd39d8ef59e3a94625
DSDimitrov19 <DSDimitrov19@codingburgas.bg>
2023-03-06 08:59:11 +0200

    Add modal for adding money to vault

--- devolut-front-end/components/vaults.tsx
+++ devolut-front-end/components/vaults.tsx
@@ -33,15 +33,21 @@ import Vault from "./vault";
 
 export default function Vaults({ session }: { session: Session | null }) {
   const [vaults, setVaults] = useState([]);
+  const [balance, setBalance] = useState(0.0);
   const [totalBalance, setTotalBalance] = useState(0);
   const [isLoading, setIsLoading] = useState(true);
   const [createVaultModalOpen, setCreateVaultModalOpen] = useState(false);
+  const [addMoneyIntoVaultModalOpen, setAddMoneyIntoVaultModalOpen] =
+    useState(false);
   const [createVaultLoading, setCreateVaultLoading] = useState(false);
-  const [hovered, setHovered] = useState('');
+  const [addMoneyIntoVaultLoading, setAddMoneyIntoVaultLoading] =
+    useState(false);
+  const [hovered, setHovered] = useState("");
   const toast = useToast();
 
   useEffect(() => {
     getVaults();
+    getBalance();
 
     setTimeout(() => {
       setIsLoading(false);
@@ -65,12 +71,29 @@ export default function Vaults({ session }: { session: Session | null }) {
 
     let totalBalance = 0;
 
-    vaults.forEach(vault => {
+    vaults.forEach((vault) => {
       totalBalance += vault.balance;
     });
 
     setVaults(vaults);
     setTotalBalance(totalBalance);
+  }
+
+  async function getBalance() {
+    const res = await fetch(process.env.BACKEND_URL + "/user/read", {
+      method: "POST",
+      body: JSON.stringify({
+        email: session?.user.dTag,
+        password: session?.user.password,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (!res.ok) return null;
+
+    const user = await res.json();
+
+    setBalance(user.balance);
   }
 
   const handleVaultCreation = async (e: any) => {
@@ -125,6 +148,7 @@ export default function Vaults({ session }: { session: Session | null }) {
   useEffect(() => {
     const interval = setInterval(async () => {
       getVaults();
+      getBalance();
     }, 5000);
 
     return () => clearInterval(interval);
@@ -175,7 +199,7 @@ export default function Vaults({ session }: { session: Session | null }) {
                 </Button>
                 <Button
                   onClick={() => {
-                    // setSendMoneyModalOpen(true);
+                    setAddMoneyIntoVaultModalOpen(true);
                   }}
                   leftIcon={<SmallAddIcon />}
                   size={"md"}
@@ -206,7 +230,12 @@ export default function Vaults({ session }: { session: Session | null }) {
               justifyItems={"flex-start"}
             >
               {vaults.map((vault, index) => (
-                <Vault key={vault.id} vault={vault} hovered={hovered} setHovered={setHovered} />
+                <Vault
+                  key={vault.id}
+                  vault={vault}
+                  hovered={hovered}
+                  setHovered={setHovered}
+                />
               ))}
             </Box>
 
@@ -320,6 +349,106 @@ export default function Vaults({ session }: { session: Session | null }) {
                           colorScheme="red"
                           width={"100%"}
                           isDisabled={createVaultLoading}
+                        >
+                          Откажи
+                        </Button>
+                      </Flex>
+                    </Flex>
+                  </form>
+                </ModalBody>
+              </ModalContent>
+            </Modal>
+
+            <Modal
+              onClose={() => {
+                setAddMoneyIntoVaultModalOpen(false);
+              }}
+              isOpen={addMoneyIntoVaultModalOpen}
+              isCentered
+            >
+              <ModalOverlay />
+              <ModalContent>
+                <ModalHeader>Добави пари</ModalHeader>
+                <ModalCloseButton />
+                <ModalBody>
+                  <form>
+                    <Flex wrap={"wrap"} direction={"column"} gap={"2"}>
+                      <Flex wrap={"wrap"}>
+                        <Text
+                          marginBottom={"1"}
+                          fontSize={"md"}
+                          fontWeight={"semibold"}
+                        >
+                          Сейф
+                        </Text>
+                        <Select
+                          width={"100%"}
+                          height={"12"}
+                          marginBottom={"2"}
+                          minWidth={"0"}
+                          fontSize={"lg"}
+                          fontWeight={"semibold"}
+                          variant="outline"
+                          colorScheme={"blue"}
+                          name="vault"
+                          required
+                          disabled={addMoneyIntoVaultLoading}
+                        >
+                          {vaults.map((vault) => (
+                            <option value={vault.id}>{vault.name}</option>
+                          ))}
+                        </Select>
+                      </Flex>
+
+                      <Flex wrap={"wrap"}>
+                        <Text
+                          marginBottom={"1"}
+                          fontSize={"md"}
+                          fontWeight={"semibold"}
+                        >
+                          Сума
+                        </Text>
+                        <NumberInput
+                          defaultValue={1}
+                          min={0.01}
+                          max={balance}
+                          precision={2}
+                          size="md"
+                          marginBottom={"2"}
+                          width={"100%"}
+                          name="amount"
+                          isDisabled={addMoneyIntoVaultLoading}
+                        >
+                          <NumberInputField />
+                          <NumberInputStepper>
+                            <NumberIncrementStepper />
+                            <NumberDecrementStepper />
+                          </NumberInputStepper>
+                        </NumberInput>
+                      </Flex>
+
+                      <Flex
+                        justify="space-between"
+                        wrap="nowrap"
+                        mb="3"
+                        mt="4"
+                        gap={"2"}
+                      >
+                        <Button
+                          colorScheme="green"
+                          type="submit"
+                          width={"100%"}
+                          isLoading={addMoneyIntoVaultLoading}
+                        >
+                          Създай
+                        </Button>
+                        <Button
+                          onClick={() => {
+                            setAddMoneyIntoVaultModalOpen(false);
+                          }}
+                          colorScheme="red"
+                          width={"100%"}
+                          isDisabled={addMoneyIntoVaultLoading}
                         >
                           Откажи
                         </Button>
