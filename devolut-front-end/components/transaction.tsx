127e7ab660c8326e8429aebcdd3559b38a243df3
DSDimitrov19 <DSDimitrov19@codingburgas.bg>
2023-03-09 08:31:39 +0200

    Adapt transaction component for vault transaction

--- devolut-front-end/components/transaction.tsx
+++ devolut-front-end/components/transaction.tsx
@@ -30,7 +30,7 @@ export default function Transaction({
       >
         <Flex alignItems={"center"} gap={"3"}>
           {(() => {
-            if (transaction.senderDTag) {
+            if (transaction.type == "user") {
               if (transaction.senderDTag == session.user.dTag) {
                 return <Box display={"flex"} alignItems={"flex-end"}>
                   <Avatar src={transaction.receiverAvatarSrc} name={transaction.receiverDTag} borderWidth={"2px"} borderColor={"whiteAlpha.50"} marginRight={"-4"} />
@@ -46,27 +46,49 @@ export default function Transaction({
                   </Box>
                 </Box> 
               }
-            } else {
+            } else if (transaction.type == "card") {
               return <Box display={"flex"} alignItems={"flex-end"}>
                 <Avatar src="https://cdn-icons-png.flaticon.com/512/504/504674.png" name={"Credit card"} borderWidth={"2px"} borderColor={"whiteAlpha.50"} marginRight={"-4"} />
                 <Box backgroundColor={"blue.300"} border='2px' borderColor={'black'} zIndex={"3"} w={"5"} h={"5"} borderRadius={"xl"} display={"flex"} alignItems={'center'} alignContent={'center'} justifyContent={'center'} justifyItems={'center'}>
                     <ArrowBackIcon color={"black"} />
                 </Box>
               </Box> 
+            } else if (transaction.type == "vault") {
+              if (transaction.receiverVaultName) {
+                return <Box display={"flex"} alignItems={"flex-end"}>
+                  <Avatar src="https://pngimg.com/d/safe_PNG33.png" name={transaction.receiverVaultName} borderWidth={"2px"} borderColor={"whiteAlpha.50"} marginRight={"-4"} />
+                  <Box backgroundColor={"blue.300"} border='2px' borderColor={'black'} zIndex={"3"} w={"5"} h={"5"} borderRadius={"xl"} display={"flex"} alignItems={'center'} alignContent={'center'} justifyContent={'center'} justifyItems={'center'}>
+                      <ArrowForwardIcon color={"black"} />
+                  </Box>
+                </Box>
+              } else {
+                return <Box display={"flex"} alignItems={"flex-end"}>
+                  <Avatar src="https://pngimg.com/d/safe_PNG33.png" name={transaction.receiverVaultName} borderWidth={"2px"} borderColor={"whiteAlpha.50"} marginRight={"-4"} />
+                  <Box backgroundColor={"blue.300"} border='2px' borderColor={'black'} zIndex={"3"} w={"5"} h={"5"} borderRadius={"xl"} display={"flex"} alignItems={'center'} alignContent={'center'} justifyContent={'center'} justifyItems={'center'}>
+                      <ArrowBackIcon color={"black"} />
+                  </Box>
+                </Box>
+              }
             }
           })()}
 
           <Flex direction={"column"}>
             <Text>
               {(() => {
-                if (transaction.senderDTag) {
+                if (transaction.type == "user") {
                   if (transaction.senderDTag == session.user.dTag) {
                     return <>До {transaction.receiverDTag}</>
                   } else {
                     return <>От {transaction.senderDTag}</>
                   }
-                } else {
+                } else if (transaction.type == "card") {
                   return <>От карта завършваща на {transaction.cardNumber.slice(-4)}</>
+                } else if (transaction.type == "vault") {
+                  if (transaction.receiverVaultName) {
+                    return <>До сейф {transaction.receiverVaultName}</>
+                  } else {
+                    return <>От сейф {transaction.senderVaultName}</>
+                  }
                 }
               })()}
             </Text>
@@ -79,14 +101,20 @@ export default function Transaction({
 
         <Text>
           {(() => {
-            if (transaction.senderDTag) {
+            if (transaction.type == "user") {
               if (transaction.senderDTag == session.user.dTag) {
                 return <>-{transaction.amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} лв</>
               } else {
                 return <>+{transaction.amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} лв</>
               }
-            } else {
+            } else if (transaction.type == "card") {
               return <>+{transaction.amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} лв</>
+            } else if (transaction.type == "vault") {
+              if (transaction.receiverVaultName) {
+                return <>+{transaction.amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} лв</>
+              } else {
+                return <>-{transaction.amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} лв</>
+              }
             }
           })()}
         </Text>
