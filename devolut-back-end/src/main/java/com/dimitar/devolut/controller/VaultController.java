884e3bcda29112063609439ae2a941f9803ec532
DSDimitrov19 <DSDimitrov19@codingburgas.bg>
2023-03-09 08:32:53 +0200

    Add vault controller

--- devolut-back-end/src/main/java/com/dimitar/devolut/controller/VaultController.java
+++ devolut-back-end/src/main/java/com/dimitar/devolut/controller/VaultController.java
@@ -2,6 +2,7 @@ package com.dimitar.devolut.controller;
 
 import com.dimitar.devolut.model.User;
 import com.dimitar.devolut.model.Vault;
+import com.dimitar.devolut.model.VaultUser;
 import com.dimitar.devolut.service.VaultService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -21,4 +22,10 @@ public class VaultController {
 
     @PostMapping("/user")
     public ResponseEntity<List<Vault>> getUserVaults(@RequestBody User user) { return vaultService.getUserVaults(user); }
+
+    @PostMapping("/deposit")
+    public ResponseEntity depositMoney(@RequestBody VaultUser vaultUser) { return vaultService.depositMoney(vaultUser); }
+
+    @PostMapping("/withdraw")
+    public ResponseEntity withdrawMoney(@RequestBody VaultUser vaultUser) { return vaultService.withdrawMoney(vaultUser); }
 }
