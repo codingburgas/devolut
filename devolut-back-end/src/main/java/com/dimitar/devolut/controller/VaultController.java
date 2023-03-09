54fd2df815b5642ecf6ea03da283a5b0a8347686
DSDimitrov19 <DSDimitrov19@codingburgas.bg>
2023-03-09 11:28:24 +0200

    Add mapping for the vault sharing

--- devolut-back-end/src/main/java/com/dimitar/devolut/controller/VaultController.java
+++ devolut-back-end/src/main/java/com/dimitar/devolut/controller/VaultController.java
@@ -2,6 +2,7 @@ package com.dimitar.devolut.controller;
 
 import com.dimitar.devolut.model.User;
 import com.dimitar.devolut.model.Vault;
+import com.dimitar.devolut.model.VaultShare;
 import com.dimitar.devolut.model.VaultUser;
 import com.dimitar.devolut.service.VaultService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,4 +29,7 @@ public class VaultController {
 
     @PostMapping("/withdraw")
     public ResponseEntity withdrawMoney(@RequestBody VaultUser vaultUser) { return vaultService.withdrawMoney(vaultUser); }
+
+    @PostMapping("/share")
+    public ResponseEntity shareVault(@RequestBody VaultShare vaultShare) { return vaultService.shareVault(vaultShare); }
 }
