bc170e492a43d1a39ea52db9138b7453f61abd53
DSDimitrov19 <DSDimitrov19@codingburgas.bg>
2023-03-09 08:32:35 +0200

    Make each vault able to call it

--- devolut-front-end/components/vault.tsx
+++ devolut-front-end/components/vault.tsx
@@ -10,106 +10,121 @@ import {
   Text,
 } from "@chakra-ui/react";
 
-export default function Vault({ vault, hovered, setHovered }) {
+export default function Vault({ vault, hovered, setHovered, setCurrentVault, setAddMoneyIntoVaultModalOpen, setTakeMoneyFromVaultModalOpen }) {
   return (
-    <Skeleton
-      isLoaded={true}
-      key={vault.id}
-      borderRadius={"md"}
-      onMouseEnter={() => setHovered(vault.name)}
-      onMouseLeave={() => setHovered("")}
-    >
-      <Box
-        display={"flex"}
-        justifyContent={"space-between"}
-        alignItems={"center"}
-        padding={"2"}
-        height={"75px"}
-        backgroundColor={"gray.600"}
+    <>
+      <Skeleton
+        isLoaded={true}
+        key={vault.id}
         borderRadius={"md"}
+        onMouseEnter={() => setHovered(vault.name)}
+        onMouseLeave={() => setHovered("")}
       >
-        <Flex
-          direction={"column"}
+        <Box
+          display={"flex"}
           justifyContent={"space-between"}
-          h="full"
-          w="20%"
+          alignItems={"center"}
+          padding={"2"}
+          height={"75px"}
+          backgroundColor={"gray.600"}
+          borderRadius={"md"}
         >
-          <Text>
-            {vault.name}{" "}
+          <Flex
+            direction={"column"}
+            justifyContent={"space-between"}
+            h="full"
+            w="20%"
+          >
+            <Text>
+              {vault.name}{" "}
+              {vault.goal ? (
+                <>
+                  -{" "}
+                  {vault.goal.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}{" "}
+                  лв
+                </>
+              ) : (
+                <></>
+              )}
+            </Text>
             {vault.goal ? (
+              <Flex direction={"column"} alignItems={"center"}>
+                <Progress
+                  value={(vault.balance * 100) / vault.goal}
+                  size="sm"
+                  borderRadius={"md"}
+                  colorScheme="blue"
+                  w="full"
+                />
+                <Text w="full">
+                  {vault.balance
+                    .toString()
+                    .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}{" "}
+                  лв ({((vault.balance * 100) / vault.goal).toFixed(1)}%)
+                </Text>
+              </Flex>
+            ) : (
               <>
-                - {vault.goal.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}{" "}
+                {vault.balance.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}{" "}
                 лв
               </>
-            ) : (
-              <></>
             )}
-          </Text>
-          {vault.goal ? (
-            <Flex direction={"column"} alignItems={"center"}>
-              <Progress
-                value={(vault.balance * 100) / vault.goal}
-                size="sm"
-                borderRadius={"md"}
-                colorScheme="blue"
-                w="full"
-              />
-              <Text w="full">
-                {vault.balance.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}{" "}
-                лв ({(vault.balance * 100) / vault.goal}%)
-              </Text>
-            </Flex>
-          ) : (
-            <>{vault.balance.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}{" "}
-            лв</>
-          )}
-        </Flex>
+          </Flex>
 
-        <ScaleFade
-          key={vault.id}
-          id={vault.name}
-          initialScale={0.9}
-          in={hovered === vault.name ? true : false}
-        >
-          <Box
-            background={"blackAlpha.700"}
-            padding={"2"}
-            width={"fit-content"}
-            borderRadius={"md"}
+          <ScaleFade
+            key={vault.id}
+            id={vault.name}
+            initialScale={0.9}
+            in={hovered === vault.name ? true : false}
           >
-            <ButtonGroup display={"flex"}>
-              <IconButton
-                colorScheme={"blue"}
-                fontSize={"md"}
-                minWidth={"8"}
-                height={"8"}
-                aria-label="Добави пари"
-                icon={<AddIcon />}
-              ></IconButton>
-              <IconButton
-                colorScheme={"blue"}
-                fontSize={"md"}
-                minWidth={"8"}
-                height={"8"}
-                aria-label="Изтегли пари"
-                icon={<MinusIcon />}
-              ></IconButton>
-              {vault.type == "shared" ? (
+            <Box
+              background={"blackAlpha.700"}
+              padding={"2"}
+              width={"fit-content"}
+              borderRadius={"md"}
+            >
+              <ButtonGroup display={"flex"}>
+                <IconButton
+                  colorScheme={"blue"}
+                  fontSize={"md"}
+                  minWidth={"8"}
+                  height={"8"}
+                  aria-label="Добави пари"
+                  icon={<AddIcon />}
+                  onClick={() => {
+                    setCurrentVault(vault)
+                    setAddMoneyIntoVaultModalOpen(true)
+                  }}
+                ></IconButton>
                 <IconButton
                   colorScheme={"blue"}
                   fontSize={"md"}
                   minWidth={"8"}
                   height={"8"}
                   aria-label="Изтегли пари"
-                  icon={<LinkIcon />}
+                  icon={<MinusIcon />}
+                  onClick={() => {
+                    setCurrentVault(vault)
+                    setTakeMoneyFromVaultModalOpen(true)
+                  }}
                 ></IconButton>
-              ) : (
-                <></>
-              )}
-            </ButtonGroup>
-          </Box>
-        </ScaleFade>
-      </Box>
-    </Skeleton>
+                {vault.type == "shared" ? (
+                  <IconButton
+                    colorScheme={"blue"}
+                    fontSize={"md"}
+                    minWidth={"8"}
+                    height={"8"}
+                    aria-label="Сподели сейф"
+                    icon={<LinkIcon />}
+                  ></IconButton>
+                ) : (
+                  <></>
+                )}
+              </ButtonGroup>
+            </Box>
+          </ScaleFade>
+        </Box>
+      </Skeleton>
+    </>
   );
 }
