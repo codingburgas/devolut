b11ee504a17c8747765e8407b1b7171a52d27ee7
DSDimitrov19 <DSDimitrov19@codingburgas.bg>
2023-03-15 09:34:35 +0200

    Implement password encryption

--- devolut-back-end/src/main/java/com/dimitar/devolut/service/UserServiceImpl.java
+++ devolut-back-end/src/main/java/com/dimitar/devolut/service/UserServiceImpl.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -13,10 +15,16 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository userRepository;
 
+    private final PasswordEncoder encoder = new BCryptPasswordEncoder();
+
     @Override
     public ResponseEntity<User> createUser(User user) {
         if (userRepository.findByEmail(user.getEmail()) != null) return new ResponseEntity<>(null, HttpStatus.FOUND);
         if (userRepository.findBydTag(user.getdTag()) != null) return new ResponseEntity<>(null, HttpStatus.IM_USED);
+
+        String hashedPassword = encoder.encode(user.getPassword());
+
+        user.setPassword(hashedPassword);
 
         return ResponseEntity.ok(userRepository.save(user));
     }
@@ -27,13 +35,22 @@ public class UserServiceImpl implements UserService {
             return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
         }
 
-        if (userRepository.findByEmail(user.getEmail()) != null && userRepository.findByEmail(user.getEmail()).getPassword().equals(user.getPassword())) {
+        if (userRepository.findByEmail(user.getEmail()) != null && encoder.matches(user.getPassword(), userRepository.findByEmail(user.getEmail()).getPassword())) {
             return ResponseEntity.ok(userRepository.findByEmail(user.getEmail()));
-        } else if (userRepository.findBydTag(user.getEmail()) != null && userRepository.findBydTag(user.getEmail()).getPassword().equals(user.getPassword())) {
+        } else if (userRepository.findBydTag(user.getEmail()) != null && encoder.matches(user.getPassword(), userRepository.findBydTag(user.getEmail()).getPassword())) {
             return ResponseEntity.ok(userRepository.findBydTag(user.getEmail()));
         }
 
         return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    public ResponseEntity<Double> getUserBalance(User user) {
+        if (userRepository.findBydTagAndIdAndPassword(user.getdTag(), user.getId(), user.getPassword()) == null) {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+
+        return ResponseEntity.ok(userRepository.findBydTagAndIdAndPassword(user.getdTag(), user.getId(), user.getPassword()).getBalance());
     }
 
     @Override
