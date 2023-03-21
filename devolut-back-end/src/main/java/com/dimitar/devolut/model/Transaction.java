d7d654c0c6977f5d2a8ddd81597072463bf5b519
DSDimitrov19 <DSDimitrov19@codingburgas.bg>
2023-03-21 10:01:43 +0200

    Add table name for the transactions

--- devolut-back-end/src/main/java/com/dimitar/devolut/model/Transaction.java
+++ devolut-back-end/src/main/java/com/dimitar/devolut/model/Transaction.java
@@ -6,6 +6,7 @@ import javax.persistence.*;
 import java.util.Date;
 
 @Entity
+@Table(name = "transactions")
 public class Transaction {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
